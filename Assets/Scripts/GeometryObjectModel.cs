fd38e5f6f5635c37c452fbc2aa5ea550a6ea0a22
HadkondVV <49709862+HadkondVV@users.noreply.github.com>
2020-11-28 11:39:51 +0200

    Sum

---
+++ Assets/Scripts/GeometryObjectModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace OBJ
+{
+    public class GeometryObjectModel : MonoBehaviour
+    {
+        int ClickCount;
+        public int MinClicksCount;
+        public int MaxClicksCount;
+        public Color ObjectColor;
+        CompositeDisposable disposables;
+        static System.Random rand = new System.Random();
+        void Start () 
+        {
+            Observable.Timer(System.TimeSpan.FromSeconds(3)).Repeat().Subscribe(_ => 
+            { 
+                this.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            }).AddTo (disposables);
+        }
+        void OnEnable() 
+        {
+            disposables = new CompositeDisposable();
+        }
+        void OnDisable() 
+        {
+            if (disposables != null) disposables.Dispose ();
+        }
+        void OnMouseDown() 
+        {
+            if(++ClickCount >= MinClicksCount && ClickCount <= MaxClicksCount) this.gameObject.GetComponent<Renderer>().material.color = ObjectColor;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjName.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OBJ
+{
+    public class ObjName : MonoBehaviour
+    {
+        public GameObject[] Objs;
+        Vector3 clickPos;
+        Plane plane;
+        GeometryObjectModel GOM;
+        void Start()
+        {
+            clickPos = Vector3.one;
+            plane = new Plane(Vector3.up, 0f);
+            ObjectName ON = JsonUtility.FromJson<ObjectName>(File.ReadAllText(Application.dataPath + @"\ObjNameFile.json"));
+            for(int i = 0; i <= 3; i += 1) Objs[i].name = ON.name[i];
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit)) if(hit.collider.name != "Terrain") return;
+                float dist;         
+                if (plane.Raycast(ray, out dist)) 
+                {
+                    clickPos = ray.GetPoint(dist);
+                    Instantiate(Objs[Random.Range(0, 4)]).transform.position = clickPos + new Vector3(0, 3, 0);
+                }
+            }
+        }
+        class ObjectName
+        {
+            public string[] name;
+        }
+    }
+}
